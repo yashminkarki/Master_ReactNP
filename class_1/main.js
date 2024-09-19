92a9cb4c287e1d0babfe9b3a654255a2cb4cded4
yashmin <yashminkarki18@gmail.com>
2024-09-19 09:10:54 -0500

    add class 1 code

---
+++ class_1/main.js
@@ -0,0 +1,33 @@
+let a = 12;
+
+let name = "ram";
+name = "asd"
+
+const age = 12
+age = 12
+
+
+console.log(a, age);
+
+const weekdays = ["monday", "tuesday", "wed","thurs", "fri"];
+
+
+
+console.log(weekdays[0]);
+
+//undefined, 
+// || ==> or, && ==> and;
+
+const myName = null;
+const name = myName || "ram" 
+
+const name2 = myName && "ram";
+
+// ? : Ternary operator
+
+//Object
+const user = {
+    name: "Yashmin",
+    job: "student"
+};
+
